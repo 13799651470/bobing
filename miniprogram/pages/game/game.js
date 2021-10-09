6b493f20a151364c55bbe1b6b440240d0d774b2d
zrabbbit <1374803499@qq.com>
2021-10-09 10:38:33 +0800

    style: change some name

--- miniprogram/pages/game/game.js
+++ miniprogram/pages/game/game.js
@@ -28,26 +28,7 @@ Page({
     t:0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    },
-
+ 
   /**
    * 生命周期函数--监听页面隐藏
    */
@@ -55,33 +36,10 @@ Page({
     this.isShow = false;
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
+/**
+   * 随机数生成以及点数图片替换函数
    */
-  onUnload: function () {
-    
-  },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  },
   randomNum: function () { 
     this.setData({
       one_count:0,
@@ -91,55 +49,66 @@ Page({
       five_count:0,
       six_count:0
     })
+
     this.setData({
       num1: parseInt(Math.random()*6,10)+1
     })
+
     this.setData({
       num2: parseInt(Math.random()*6,10)+1
     })
+
     this.setData({
       num3: parseInt(Math.random()*6,10)+1
     })
+
     this.setData({
       num4: parseInt(Math.random()*6,10)+1
     })
+
     this.setData({
       num5: parseInt(Math.random()*6,10)+1
     })
+
     this.setData({
       num6: parseInt(Math.random()*6,10)+1
     })
-    // console.log(parseInt(Math.random()*6,10)+1);
+
     if(this.data.num1==1){
       this.setData({
         image1: '../../images/one_image.png',
         one_count:this.data.one_count+1
       })
     }
+
     if(this.data.num1==2){
       this.setData({
         image1: '../../images/two_image.png',
         two_count:this.data.two_count+1
       })
     }
+
     if(this.data.num1==3){
       this.setData({
         image1: '../../images/three_image.png',
         three_count:this.data.three_count+1
       })
     }
+
     if(this.data.num1==4){
       this.setData({
         image1: '../../images/four_image.png',
         four_count:this.data.four_count+1
       })
     }
+
     if(this.data.num1==5){
       this.setData({
         image1: '../../images/five_image.png',
         five_count:this.data.five_count+1
       })
     }
+
     if(this.data.num1==6){
       this.setData({
         image1: '../../images/six_image.png',
@@ -153,30 +122,35 @@ Page({
         one_count:this.data.one_count+1
       })
     }
+
     if(this.data.num2==2){
       this.setData({
         image2: '../../images/two_image.png',
         two_count:this.data.two_count+1
       })
     }
+
     if(this.data.num2==3){
       this.setData({
         image2: '../../images/three_image.png',
         three_count:this.data.three_count+1
       })
     }
+
     if(this.data.num2==4){
       this.setData({
         image2: '../../images/four_image.png',
         four_count:this.data.four_count+1
       })
     }
+
     if(this.data.num2==5){
       this.setData({
         image2: '../../images/five_image.png',
         five_count:this.data.five_count+1
       })
     }
+
     if(this.data.num2==6){
       this.setData({
         image2: '../../images/six_image.png',
@@ -190,30 +164,35 @@ Page({
         one_count:this.data.one_count+1
       })
     }
+
     if(this.data.num3==2){
       this.setData({
         image3: '../../images/two_image.png',
         two_count:this.data.two_count+1
       })
     }
+
     if(this.data.num3==3){
       this.setData({
         image3: '../../images/three_image.png',
         three_count:this.data.three_count+1
       })
     }
+
     if(this.data.num3==4){
       this.setData({
         image3: '../../images/four_image.png',
         four_count:this.data.four_count+1
       })
     }
+
     if(this.data.num3==5){
       this.setData({
         image3: '../../images/five_image.png',
         five_count:this.data.five_count+1
       })
     }
+
     if(this.data.num3==6){
       this.setData({
         image3: '../../images/six_image.png',
@@ -227,30 +206,35 @@ Page({
         one_count:this.data.one_count+1
       })
     }
+
     if(this.data.num4==2){
       this.setData({
         image4: '../../images/two_image.png',
         two_count:this.data.two_count+1
       })
     }
+
     if(this.data.num4==3){
       this.setData({
         image4: '../../images/three_image.png',
         three_count:this.data.three_count+1
       })
     }
+
     if(this.data.num4==4){
       this.setData({
         image4: '../../images/four_image.png',
         four_count:this.data.four_count+1
       })
     }
+
     if(this.data.num4==5){
       this.setData({
         image4: '../../images/five_image.png',
         five_count:this.data.five_count+1
       })
     }
+
     if(this.data.num4==6){
       this.setData({
         image4: '../../images/six_image.png',
@@ -264,30 +248,35 @@ Page({
         one_count:this.data.one_count+1
       })
     }
+
     if(this.data.num5==2){
       this.setData({
         image5: '../../images/two_image.png',
         two_count:this.data.two_count+1
       })
     }
+
     if(this.data.num5==3){
       this.setData({
         image5: '../../images/three_image.png',
         three_count:this.data.three_count+1
       })
     }
+
     if(this.data.num5==4){
       this.setData({
         image5: '../../images/four_image.png',
         four_count:this.data.four_count+1
       })
     }
+
     if(this.data.num5==5){
       this.setData({
         image5: '../../images/five_image.png',
         five_count:this.data.five_count+1
       })
     }
+
     if(this.data.num5==6){
       this.setData({
         image5: '../../images/six_image.png',
@@ -301,30 +290,35 @@ Page({
         one_count:this.data.one_count+1
       })
     }
+
     if(this.data.num6==2){
       this.setData({
         image6: '../../images/two_image.png',
         two_count:this.data.two_count+1
       })
     }
+
     if(this.data.num6==3){
       this.setData({
         image6: '../../images/three_image.png',
         three_count:this.data.three_count+1
       })
     }
+
     if(this.data.num6==4){
       this.setData({
         image6: '../../images/four_image.png',
         four_count:this.data.four_count+1
       })
     }
+
     if(this.data.num6==5){
       this.setData({
         image6: '../../images/five_image.png',
         five_count:this.data.five_count+1
       })
     }
+
     if(this.data.num6==6){
       this.setData({
         image6: '../../images/six_image.png',
@@ -332,6 +326,11 @@ Page({
       })
     }
   },
+
+/**
+   * 结果检测函数
+   */
+
   check: function () {
     if(this.data.four_count==1){
     getApp().globalData.xiucai_sum = getApp().globalData.xiucai_sum + 1;
@@ -397,6 +396,7 @@ Page({
         }
         
     }
+
     else if(this.data.four_count==6){
     getApp().globalData.zhuangyuan_sum = getApp().globalData.zhuangyuan_sum + 1;
     wx.showModal({
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,34 +4,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-    first_background_image : "/image/First_bg.jpg"
+    
   },
 
 
 
-  click1 : function () {
-    wx.navigateTo({
-      url: '../game/game',
-    })
+  onClick1 : function () {
+      wx.navigateTo({
+          url: '../game/game'
+      })
   },
 
-  click2 : function () {
-    wx.navigateTo({
-      url: '../demo1/demo1',
-    })
+  onClick2 : function () {
+      wx.navigateTo({
+          url: '../demo1/demo1'
+      })
   },
 
-  click3 : function () {
-    wx.navigateTo({
-      url: '../rules/rules',
-    })
+  onClick3 : function () {
+      wx.navigateTo({
+          url: '../rules/rules'
+      })
   },
 
-  click4 : function () {
-    wx.navigateTo({
-      url: '../setprize/setprize',
-    })
+  onClick4 : function () {
+      wx.navigateTo({
+          url: '../setprize/setprize'
+      })
   }
-
 
 })
